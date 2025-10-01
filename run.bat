5cbe2e028c8084cbb9a9d786fb027ccc4c73c854
Marawan Mokhtar <105330382+marawan10@users.noreply.github.com>
2025-10-01 07:21:12 -0700

    Delete run.bat

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Start backend
-start cmd /k "cd /d C:\Users\Marwan Mokhtar\Desktop\calories-tracker-main\backend && npm run dev"
-
-REM Wait 2 seconds
-timeout /t 2 /nobreak >nul
-
-REM Start frontend
-start cmd /k "cd /d C:\Users\Marwan Mokhtar\Desktop\calories-tracker-main\frontend && npm run dev"
